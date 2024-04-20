b92b82fb021ad5d2a9201309d90681438fedc356
unknown <unknow@gmail.com>
2024-04-20 17:03:42 +0100

    update admin

--- config/env.ts
+++ config/env.ts
@@ -1,18 +1,15 @@
 import dotenv from "dotenv";
 
-const envs = dotenv.config({ path: ".env" });
+const envs = dotenv.config({path: ".env"});
 
 const isDev = process.env.NODE_ENV === "development";
 
 const env = {
-   BASE_URL: envs.parsed?.BASE_URL || "",
-   isDev: isDev,
+    BASE_URL: envs.parsed?.BASE_URL || "",
+    isDev: isDev,
 };
 
-export const supabaseServiceRole = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE || "";
 
-
-console.log(env,"env--+++");
 export default env;
 
 
