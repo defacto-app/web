778cecf89cb0005531ed4e8759bd1a34d38818bf
unknown <unknow@gmail.com>
2024-04-27 23:14:51 +0100

    fix doten issue

--- config/env.ts
+++ config/env.ts
@@ -1,14 +1,11 @@
-import dotenv from "dotenv";
 
-const envs = dotenv.config({path: ".env"});
 
 const isDev = process.env.NODE_ENV === "development";
 
 const env = {
-    BASE_URL: envs.parsed?.BASE_URL || "",
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "",
     isDev: isDev,
 };
-
 
 export default env;
 
