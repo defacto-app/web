ffa060bb8ddf51688ba11c52622da6f1c3a09f64
unknown <unknow@gmail.com>
2024-04-30 17:44:57 +0100

    updated swagger

---
+++ app/admin/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function AdminIndex() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="flex justify-center">
+          <h1> Welcome to admin Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const runtime = 'edge';
