f0207b8ee32a1accfecd5ab1dc6313038ef88e5f
MadFury94 <onochieazukaeme@gmail.com>
2024-05-01 07:14:52 +0100

    refactor for mobile

--- components/home/BecomeRider.tsx
+++ components/home/BecomeRider.tsx
@@ -28,52 +28,7 @@ export default function BecomeRider() {
           </div>
 
           <div className="relative  lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl">
-            {/* <div className="absolute inset-0 hidden overflow-hidden rounded-3xl lg:block" aria-hidden="true">
-              <svg
-                className="absolute bottom-full left-full -translate-x-2/3 translate-y-1/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0"
-                width={404}
-                height={384}
-                fill="none"
-                viewBox="0 0 404 384"
-                aria-hidden="true"
-              >
-                <defs>
-                  <pattern
-                    id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect x={0} y={0} width={4} height={4} className="text-indigo-500" fill="currentColor" />
-                  </pattern>
-                </defs>
-                <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-              </svg>
-              <svg
-                className="absolute top-full -translate-x-1/3 -translate-y-1/3 transform xl:-translate-y-1/2"
-                width={404}
-                height={384}
-                fill="none"
-                viewBox="0 0 404 384"
-                aria-hidden="true"
-              >
-                <defs>
-                  <pattern
-                    id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect x={0} y={0} width={4} height={4} className="text-indigo-500" fill="currentColor" />
-                  </pattern>
-                </defs>
-                <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-              </svg>
-            </div> */}
+
             <div className="relative mx-auto max-w-md space-y-6 px-6 py-12 sm:max-w-3xl sm:py-16 lg:col-span-6 lg:col-start-4 lg:max-w-none lg:p-0">
               <h2 className="text-4xl sm:text-6xl font-bold tracking-tight " id="join-heading">
               Become a Rider/Driver at <span className='text-primary-600'>Defacto</span>
@@ -84,10 +39,11 @@ export default function BecomeRider() {
 
               <div className='py-1'>
               <Link href={href}>
-                    <Button variant="primary2">
+                    <Button variant="primary" className='py-5 w-full px-24'>
                       Become Rider
                     </Button>
                 </Link>
+
               </div>
             </div>
           </div>
