9163a11e37c964518b996f6974e23b92b9a834b7
MadFury94 <onochieazukaeme@gmail.com>
2024-05-01 12:53:10 +0100

    save

--- components/about/HeroSection.tsx
+++ components/about/HeroSection.tsx
@@ -36,7 +36,7 @@ export default function HeroSection () {
         alt={logo.alt}
         width={100}
         height={100}
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "100%", height: "auto" }}
       />
     </div>
   ));
