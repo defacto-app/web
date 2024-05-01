5397be8f2b6f44ac83abacf1e4741e31e4c1cb30
unknown <unknow@gmail.com>
2024-05-01 02:57:10 +0100

    partial authentication

---
+++ app/lib.ts
@@ -0,0 +1,43 @@
+import {cookies} from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
+import env from "@/config/env";
+
+
+export async function logout() {
+    // Destroy the session
+    cookies().set("session", "", {expires: new Date(0)});
+}
+
+
+export async function authenticate(data: any) {
+
+
+    // ping the server to check if the token is valid
+
+    const result = await fetch(`${env.BASE_URL}/auth/ping`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${data}`
+        },
+
+    })
+
+
+    console.log("checking auth url")
+
+    const _data = await result.json()
+
+    console.log(_data, "checking result")
+
+    if (_data.error) {
+        return false
+    }
+
+    if (_data.success) {
+        return true
+    }
+
+
+}
+
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,25 @@
 import type {NextRequest} from 'next/server'
 import {NextResponse} from 'next/server'
+import {authenticate} from "@/app/lib";
 
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 
     const authToken = request.cookies.get("auth-token")
 
-    // const isAuthenticated = authenticate(request)
-    // If the user is authenticated, continue as normal
-    if (authToken) {
+    const isAuthenticated = await authenticate(authToken?.value)
+    const url = request.nextUrl.clone()
+
+
+    if (isAuthenticated) {
         return NextResponse.next()
     }
 
     // If the user is not authenticated, redirect to log in
 
-    const url = request.nextUrl.clone()
     url.pathname = '/admin/auth/'
     return NextResponse.rewrite(url)
-
-
-    // const  isLoggedIn = true
-
-    // set authorization  headers
 
 
 }
