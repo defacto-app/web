e3fcc6b473eb3efc206cf635c99ec8b3d152d740
MadFury94 <onochieazukaeme@gmail.com>
2024-05-01 07:25:08 +0100

    Update Header
    Refactor button

--- components/Header.tsx
+++ components/Header.tsx
@@ -104,17 +104,12 @@ export default function Header() {
       >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center gap-x-6">
+          <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Image className="h-8 w-auto" src="/logo.png" alt="" width={50} height={50} />
             </a>
-            <a
-              href="#"
-            >
-            <Button variant="primary"> Get Started</Button>
 
-            </a>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -137,6 +132,12 @@ export default function Header() {
                   </a>
                 ))}
               </div>
+              <a
+              href="#"
+            >
+            <Button variant="primary"> Get Started</Button>
+
+            </a>
             </div>
           </div>
         </Dialog.Panel>
