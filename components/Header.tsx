ab6694bfcc1579f4b64812fb4cfa4f9b4c4db74f
MadFury94 <onochieazukaeme@gmail.com>
2024-05-02 09:19:59 +0100

    added framer motion to navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,14 @@
-"use client";
+"use client"
 import { useState, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
-import {
-  SquareMenu,
-  X,
-  ShoppingBag,
-  User,
-  UserRoundCogIcon,
-  Menu,
-} from "lucide-react";
+import { X, Menu, UserRoundCogIcon } from "lucide-react";
 import UserAuth from "./UserAuth";
 import { UserProvider } from "@/app/provider/auth.context";
 import env from "@/config/env";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 interface NavigationItem {
   name: string;
@@ -45,45 +39,41 @@ export default function Header() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <header
-      className={`bg-white ${isSticky ? "fixed top-0 left-0 right-0 z-50" : ""}`}
-    >
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex lg:flex-1">
-            <Link href="/">
-              <span className="sr-only">Your Company</span>
-              <Image
-                className="h-14 w-auto"
-                src="/logo.png"
-                alt=""
-                width={100}
-                height={100}
-              />
-            </Link>
-        </div>
+    <header className={`bg-white ${isSticky ? "fixed top-0 left-0 right-0 z-50" : ""}`}>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+          <Link href="/">
+            <span className="sr-only">Your Company</span>
+            <Image
+              className="h-14 w-auto"
+              src="/logo.png"
+              alt=""
+              width={100}
+              height={100}
+            />
+          </Link>
+        </motion.div>
         <div className="hidden lg:flex lg:gap-x-12 bg-gray-50 px-8 py-2 rounded-full">
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-sm  leading-6 text-gray-900"
+              className="text-sm leading-6 text-gray-900"
             >
               {item.name}
             </a>
           ))}
         </div>
         <div className="flex flex-1 items-center justify-end gap-x-6">
-
           {env.isDev && (
-            <Link href="/admin">
-              <UserRoundCogIcon className="text-red-500" size={20} />
-            </Link>
+            <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+              <Link href="/admin">
+                <UserRoundCogIcon className="text-red-500" size={20} />
+              </Link>
+            </motion.div>
           )}
-
           <UserProvider>
             <UserAuth />
           </UserProvider>
@@ -95,10 +85,7 @@ export default function Header() {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Menu
-              className="h-6 w-6 text-primary-600"
-              aria-hidden="true"
-            />
+            <Menu className="h-6 w-6 text-primary-600" aria-hidden="true" />
           </button>
         </div>
       </nav>
@@ -122,14 +109,17 @@ export default function Header() {
               />
             </Link>
 
-            <button
+            <motion.button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5 }}
             >
               <span className="sr-only">Close menu</span>
               <X className="h-6 w-6 text-primary-600" aria-hidden="true" />
-            </button>
+            </motion.button>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
@@ -145,7 +135,9 @@ export default function Header() {
                 ))}
               </div>
               <a href="#">
-                <Button variant="primary"> Get Started</Button>
+                <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <Button variant="primary"> Get Started</Button>
+                </motion.div>
               </a>
             </div>
           </div>
