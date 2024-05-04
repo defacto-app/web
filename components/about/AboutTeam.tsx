13897f4f1f670c212e9ed7846cf24956d2f5cbdf
MadFury94 <onochieazukaeme@gmail.com>
2024-05-04 11:54:01 +0100

    adjusted image <ul>

--- components/about/AboutTeam.tsx
+++ components/about/AboutTeam.tsx
@@ -17,10 +17,10 @@ export default function AboutTeam() {
   ]
   return (
     <div>
-      <div className="mx-auto mt-7 max-w-7xl px-6 sm:mt-48 lg:px-8">
+      <div className="mx-auto max-w-7xl px-6 grid justify-items-center sm:mt-48 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
 
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-black sm:text-6xl">
+          <h1 className="mt-10 text-4xl text-center font-bold tracking-tight text-black sm:text-6xl">
                   The Team
                 </h1>
 
@@ -30,18 +30,23 @@ export default function AboutTeam() {
             </p>
           </div>
           <ul
+          key={1}
             role="list"
-            className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+            className="mx-auto justify-items-center mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center "
           >
             {team.map((person) => (
-              <li key={person.name}>
-               <Image
+              <div className='grid place-content-center '>
+                <li key={person.name} >
+              <div>
+              <Image
                className="mx-auto h-24 w-24 rounded-full" src={person.imageUrl} alt="team"
                 width={100}
                 height={100} />
+              </div>
                 <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
                 <p className="text-sm leading-6 text-gray-600">{person.role}</p>
               </li>
+              </div>
             ))}
           </ul>
         </div>
