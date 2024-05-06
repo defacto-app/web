227201f8cec9f14825e8d896f7bad8d73a106d3e
MadFury94 <onochieazukaeme@gmail.com>
2024-05-06 08:44:18 +0100

    refactor phone number field

--- components/user/EditUserNumberModal.tsx
+++ components/user/EditUserNumberModal.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from 'react';
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -8,46 +8,52 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/dialog";
 
 export default function EditUserNumberModal() {
   return (
     <div>
       <div>
         <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="link">Edit</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit</DialogTitle>
-          <DialogDescription>
-            Change your phone number here and click save when done
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Call Line
-            </Label>
-            <Input
-            className='w-full'
-            type="tel"
+          <DialogTrigger asChild>
+            <Button variant="link">Edit</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit</DialogTitle>
+              <DialogDescription>
+                Change your phone number here and click save when done
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="items-center gap-4">
+                <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+                  Phone Number
+                </label>
+                <div className="relative mt-2 rounded-md shadow-sm">
+                  <div className="absolute inset-y-0 left-0 flex items-center">
+                    <label htmlFor="country" className="sr-only">
+                      Country
+                    </label>
+
+                      <option className='text-primary-600'>NG</option>
+                  </div>
+                  <input
+                    type="tel"
                     name="phone-number"
                     id="phone-number"
-                    autoComplete="tel"
+                    className="block w-full rounded-md border-gray-300 py-1.5 pl-16 pr-3 text-gray-900 placeholder-gray-400 focus:ring-indigo-500 focus:border-indigo-500 focus:ring focus:ring-opacity-50 sm:text-sm"
+                    placeholder="+234 (81) 987-6543"
                   />
-          </div>
-
-        </div>
-        <DialogFooter>
-          <Button type="submit" variant="primary">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+                </div>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit" variant="primary">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
