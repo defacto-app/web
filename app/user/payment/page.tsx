907f203db39b2151316022a2f47e9fd501d367af
MadFury94 <onochieazukaeme@gmail.com>
2024-05-08 02:54:14 +0100

    added payment image

--- app/user/payment/page.tsx
+++ app/user/payment/page.tsx
@@ -1,23 +1,128 @@
+import DeliveryFee from "@/components/user/DeliveryFee";
+import {
+  ChevronRight,
+  LocateFixedIcon,
+  LocateIcon,
+  MapIcon,
+  UserIcon,
+} from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <div className="grid h-72 grid-cols-10 ">
-        {/* image side */}
-        <div className="col-span-4 bg-green-800">Image</div>
+      <div className="grid bg-white grid-cols-10">
+        {/* video side */}
+        <div className="col-span-4   flex flex-col justify-end">
 
-        {/* review & payment side */}
-        <div className="col-span-6  ">
-          <div className="  grid ">
-            <div className="grid grid-cols-10">
-              <h1>Review & Payment</h1>
-            <div className=" h-72 col-span-5  bg-primary-600">Sender</div>
-            <div className=" h-72  col-span-5 bg-gray-800">Receiver</div>
-            </div>
+          <div>
+          <Image
+                className="object-contain max-w-full max-h-full"
+                src="/dispatch3.png"
+                alt="team"
+                width={1500}
+                height={2500}
+              />
           </div>
         </div>
+
+        {/* review & payment side */}
+        <div className="col-span-6 bg-white container">
+          <div className="">
+            <div className="mb-4 mt-10 p-10">
+              <h1 className="font-bold text-4xl sm:text-3xl">
+                Review & Payment
+              </h1>
+            </div>
+            <div className="grid grid-cols-10 gap-x-3 container">
+              <div className="col-span-5">
+                <div>
+                  <div className="bg-primary-300 p-5 rounded-lg mb-4 mt-4">
+                    <h1 className=" font-semibold text-xl">Pickup Details</h1>
+                  </div>
+                  <div className="bg-[#FFFBFE] mb-4 mt-2">
+                    <div className="grid grid-cols-3 items-center ">
+                      <div className="">
+                        <UserIcon />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-xl">Sender</h1>
+                        <p>Username</p>
+                        <p>UserPhone</p>
+                        <p>UserEmail</p>
+                      </div>
+                      <div>
+                        <ChevronRight />
+                      </div>
+                    </div>
+                  </div>{" "}
+                  <div className="bg-[#FFFBFE] mb-4 mt-2">
+                    <div className="grid grid-cols-3 items-center ">
+                      <div className="">
+                        <MapIcon />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-xl">
+                          Pickup Address
+                        </h1>
+                        <p>UserAddress</p>
+                      </div>
+                      <div>
+                        <ChevronRight />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>{" "}
+              <div className="col-span-5">
+                <div>
+                  <div className="bg-primary-300 p-5 rounded-lg mb-4 mt-4">
+                    <h1 className=" font-semibold text-xl">Drop-Off Details</h1>
+                  </div>
+                  <div className="bg-[#FFFBFE] mb-4 mt-2">
+                    <div className="grid grid-cols-3 items-center ">
+                      <div className="">
+                        <UserIcon />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-xl">Receiver</h1>
+                        <p>Username</p>
+                        <p>UserPhone</p>
+                        <p>UserEmail</p>
+                      </div>
+                      <div>
+                        <ChevronRight />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-[#FFFBFE] mb-4 mt-2">
+                  <div className="grid grid-cols-3 items-center ">
+                    <div className="">
+                      <MapIcon />
+                    </div>
+                    <div>
+                      <h1 className="font-semibold text-xl">Pickup Address</h1>
+                      <p>UserAddress</p>
+                    </div>
+                    <div>
+                      <ChevronRight />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container grid grid-cols-10">
+        <div className="col-span-2"></div>
+        <div className="col-span-8 p-20">
+          <DeliveryFee />
+        </div>
       </div>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ components/user/GifSection.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+import gif from "/dispatch.gif"
+
+export default function GifSection() {
+  return (
+    <div>
+      <div className="relative w-full h-screen overflow-hidden">
+      <Image
+        src={gif}
+        alt="Background GIF"
+        className="object-cover w-full h-full"
+        width={1000}
+        height={1000}
+      />
+    </div>
+    </div>
+  )
+}
