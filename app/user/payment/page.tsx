7709c5f351dbb10ea8aee5cf50ae35fd612a7af3
MadFury94 <onochieazukaeme@gmail.com>
2024-05-13 00:36:32 +0100

    payment modal

--- app/user/payment/page.tsx
+++ app/user/payment/page.tsx
@@ -1,23 +1,19 @@
 import DeliveryFee from "@/components/user/DeliveryFee";
 import DropoffModal from "@/components/user/DropoffModal";
+import { PaymentModal } from "@/components/user/PaymentModal";
 import PaymentOptions from "@/components/user/PaymentOptions";
 import PickupModal from "@/components/user/PickupModal";
 import ReceiverModal from "@/components/user/ReceiverModal";
 import SenderModal from "@/components/user/SenderModal";
-import {
-  ChevronRight,
-
-  MapIcon,
-  UserIcon,
-} from "lucide-react";
+import { ChevronRight, MapIcon, UserIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-
 export default function page() {
-
   const truncateText = (text: string, maxLength: number): string => {
-    return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+    return text.length > maxLength
+      ? `${text.substring(0, maxLength)}...`
+      : text;
   };
   const senderAddress = "1234 Sender Street, City, Country";
   const receiverAddress = "5678 Receiver Street, City, Country";
@@ -26,18 +22,17 @@ export default function page() {
     <div>
       <div className="grid  lg:grid-cols-10">
         <div className="col-span-4 bg-[#FFFBFB] flex flex-col justify-end">
-
-         <div className="relative">
-         <div className="absolute bottom-1">
-          <Image
+          <div className="relative">
+            <div className="absolute bottom-1">
+              <Image
                 className="object-contain max-w-full h-[500px]"
                 src="/dispatch.svg"
                 alt="team"
                 width={800}
                 height={1000}
               />
+            </div>
           </div>
-         </div>
         </div>
 
         {/* review & payment side */}
@@ -52,85 +47,87 @@ export default function page() {
               <div className="col-span-5">
                 <div>
                   <div className="bg-primary-600 p-5 rounded-lg mb-4 mt-4">
-                    <h1 className=" font-semibold text-xl text-gray-200">Pickup Details</h1>
+                    <h1 className=" font-semibold text-xl text-gray-200">
+                      Pickup Details
+                    </h1>
                   </div>
                   <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
                     <div className="grid grid-cols-3 items-center ">
-
                       <div className="col-span-2">
                         <h1 className="font-semibold text-base">Sender Name</h1>
                         <p>Sender Call line</p>
                       </div>
                       <div className="col-span-1">
-                        <SenderModal/>
+                        <SenderModal />
                       </div>
                     </div>
                   </div>{" "}
                   <div className=" mb-4 mt-2">
-                  <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
-                    <div className="grid grid-cols-3 items-center justify-between ">
-
-                      <div className="col-span-2">
-                      <h1 className="font-semibold text-base">Pick Up Address</h1>
+                    <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
+                      <div className="grid grid-cols-3 items-center justify-between ">
+                        <div className="col-span-2">
+                          <h1 className="font-semibold text-base">
+                            Pick Up Address
+                          </h1>
                           <p>{truncateText(senderAddress, 10)}</p>
-                      </div>
-                      <div className="col-span-1">
-                        <PickupModal/>
+                        </div>
+                        <div className="col-span-1">
+                          <PickupModal />
+                        </div>
                       </div>
                     </div>
-                  </div>
                   </div>
                 </div>
               </div>{" "}
               <div className="col-span-5">
                 <div>
                   <div className="bg-primary-600 p-5 rounded-lg mb-4 mt-4">
-                    <h1 className=" font-semibold text-xl text-gray-200">Drop-Off Details</h1>
+                    <h1 className=" font-semibold text-xl text-gray-200">
+                      Drop-Off Details
+                    </h1>
                   </div>
                   <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
                     <div className="grid grid-cols-3 items-center ">
-
                       <div className="col-span-2">
                         <h1 className="font-semibold text-base">Receiver</h1>
                         <p>UserPhone</p>
                       </div>
                       <div className="col-span-1">
-                        <ReceiverModal/>
+                        <ReceiverModal />
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
-                    <div className="grid grid-cols-3 items-center ">
-
-                      <div className="col-span-2">
-                      <h1 className="font-semibold text-base">Drop-Off Address</h1>
-                        <p>{truncateText(receiverAddress, 10)}</p>
-
-                      </div>
-                      <div className="col-span-1">
-                        <DropoffModal/>
-                      </div>
+                  <div className="grid grid-cols-3 items-center ">
+                    <div className="col-span-2">
+                      <h1 className="font-semibold text-base">
+                        Drop-Off Address
+                      </h1>
+                      <p>{truncateText(receiverAddress, 10)}</p>
+                    </div>
+                    <div className="col-span-1">
+                      <DropoffModal />
                     </div>
                   </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="grid place-content-center">
-          <div className="bg-[#FFFBFB] mb-4 mt-2 p-10">
-          <PaymentOptions/>
-
-</div>
+            <div className="bg-[#FFFBFB] mb-4 mt-2 py-10 px-4 w-96">
+              <PaymentOptions />
+              <PaymentModal/>
+            </div>
           </div>
-          <div className="container grid grid-cols-10">
-        <div className="col-span-2"></div>
-        <div className="col-span-8 p-20">
-          <DeliveryFee />
+          <div className="container grid lg:grid-cols-10">
+            <div className="col-span-2"></div>
+            <div className="col-span-8 p-20">
+              <DeliveryFee />
+            </div>
+          </div>
         </div>
       </div>
-        </div>
-      </div>
-
     </div>
   );
 }
--- components/user/PaymentOptions.tsx
+++ components/user/PaymentOptions.tsx
@@ -16,23 +16,22 @@ export default function PaymentOptions() {
     <div>
       <div className="">
         <Select>
-          <SelectTrigger className="w-auto">
+          <SelectTrigger className="w-full">
             <SelectValue placeholder="Payment Method" />
 
           </SelectTrigger>
-          <SelectContent className="">
+          <SelectContent className="max-w-max">
             <SelectGroup className="bg-[#FFFBFB] py-10 max-w-4xl">
               <SelectLabel>Choose one</SelectLabel>
               <SelectItem className=" rounded-xl" value="cash">
-                <div className="flex px-4 py-6 gap-5">
-
-                  <div>
-                    <h1>Pay with cash</h1>
-                  </div>
-                  <div>
-                    <Banknote className="text-primary-600" />
-                  </div>
-                </div>
+              <div className="flex items-center justify-between px-4 py-6 gap-5">
+  <div>
+    <h1>Pay with cash</h1>
+  </div>
+  <div>
+    <Banknote className="text-primary-600" />
+  </div>
+</div>
               </SelectItem>
               <SelectItem className="" value="card">
                 <div className="flex px-4 py-6 justify-between gap-5">
