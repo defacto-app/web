b637bfec75c3f3a39d35edb56df773d8878036db
MadFury94 <onochieazukaeme@gmail.com>
2024-05-14 10:25:02 +0100

    added sheet

--- app/user/payment/page.tsx
+++ app/user/payment/page.tsx
@@ -1,13 +1,14 @@
+"use client";
+
 import DeliveryFee from "@/components/user/DeliveryFee";
 import DropoffModal from "@/components/user/DropoffModal";
-import { PaymentModal } from "@/components/user/PaymentModal";
-import PaymentOptions from "@/components/user/PaymentOptions";
+import PaymentModal from "@/components/user/PaymentModal";
 import PickupModal from "@/components/user/PickupModal";
 import ReceiverModal from "@/components/user/ReceiverModal";
 import SenderModal from "@/components/user/SenderModal";
-import { ChevronRight, MapIcon, UserIcon } from "lucide-react";
+import { BanknoteIcon, CheckCircleIcon, CreditCardIcon } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function page() {
   const truncateText = (text: string, maxLength: number): string => {
@@ -18,12 +19,18 @@ export default function page() {
   const senderAddress = "1234 Sender Street, City, Country";
   const receiverAddress = "5678 Receiver Street, City, Country";
 
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+
+  const handleSelectPaymentMethod = (paymentMethod: any) => {
+    setSelectedPaymentMethod(paymentMethod);
+  };
+
   return (
     <div>
-      <div className="grid  lg:grid-cols-10">
-        <div className="col-span-4 bg-[#FFFBFB] flex flex-col justify-end">
+      <div className="grid h-[750px] lg:grid-cols-10 overflow-hidden">
+        <div className="col-span-4 bg-[#FFFBFB] flex flex-col justify-end overflow-auto">
           <div className="relative">
-            <div className="absolute bottom-1">
+            <div className="">
               <Image
                 className="object-contain max-w-full h-[500px]"
                 src="/dispatch.svg"
@@ -36,7 +43,7 @@ export default function page() {
         </div>
 
         {/* review & payment side */}
-        <div className="col-span-6 bg-white container">
+        <div className="col-span-6 bg-[#FFFBFB] container overflow-auto">
           <div className="">
             <div className="mb-4 mt-10 p-10">
               <h1 className="font-bold text-4xl sm:text-3xl">
@@ -116,13 +123,69 @@ export default function page() {
           </div>
           <div className="grid place-content-center">
             <div className="bg-[#FFFBFB] mb-4 mt-2 py-10 px-4 w-96">
-              <PaymentOptions />
-              <PaymentModal/>
+              <PaymentModal />
+              {/* <div>
+                <div>
+                  <div className="bg-[#FFFBFB] sm:max-w-[425px] rounded-xl">
+                    <div>
+                    <div className="bg-primary-600 p-5 rounded-lg mb-4 mt-4">
+                    <h1 className=" font-semibold text-xl text-gray-200">
+                      Payment
+                    </h1>
+                  </div>
+                    </div>
+                    <div>
+                      <div
+                        className={`grid grid-cols-6 bg-white rounded-xl mb-5 mt-2 p-4 ${
+                          selectedPaymentMethod === "cash"
+                            ? "ring ring-primary-600"
+                            : ""
+                        }`}
+                        onClick={() => handleSelectPaymentMethod("cash")}
+                      >
+                        <div className="col-span-1">
+                          <BanknoteIcon className="text-primary-600" />
+                        </div>
+                        <div className="col-span-4">
+                          <h1>Pay with Cash</h1>
+                        </div>
+                        <div className="col-span-1">
+                          {selectedPaymentMethod === "cash" && (
+                            <CheckCircleIcon className=" text-primary-600" />
+                          )}
+                        </div>
+                      </div>
+                      <div
+                        className={`grid grid-cols-6 bg-white rounded-xl mb-5 mt-2 p-4 ${
+                          selectedPaymentMethod === "card"
+                            ? "ring ring-primary-600"
+                            : ""
+                        }`}
+                        onClick={() => handleSelectPaymentMethod("card")}
+                      >
+                        {" "}
+                        <div className="col-span-1">
+                          <CreditCardIcon className="text-primary-600" />
+                        </div>
+                        <div className="col-span-4">
+                          <h1>Pay with Card</h1>
+                        </div>
+                        <div className="col-span-1">
+                          {selectedPaymentMethod === "card" && (
+                            <CheckCircleIcon className=" text-primary-600" />
+                          )}
+                        </div>
+                      </div>
+                    </div>{" "}
+                  </div>
+                </div>
+              </div> */}
+
             </div>
           </div>
           <div className="container grid lg:grid-cols-10">
-            <div className="col-span-2"></div>
-            <div className="col-span-8 p-20">
+            {/* <div className="col-span-2"></div> */}
+            <div className="col-span-10 p-20">
               <DeliveryFee />
             </div>
           </div>
