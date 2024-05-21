4764470ee9315f36e0a8b8bab2023181a8d41be7
MadFury94 <onochieazukaeme@gmail.com>
2024-05-21 11:39:25 +0100

    change

--- components/user/HeroImage.tsx
+++ components/user/HeroImage.tsx
@@ -1,36 +1,38 @@
 import Image from "next/image";
 import React from "react";
-import { ArrowRightIcon, CircleArrowRight } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import PaymentSheet from "./PaymentSheet";
 
 export default function HeroImage() {
   return (
-    <div className="bg-primary-200 rounded-xl w-[600px] h-[300px]">
-      <div className="container  mx-auto py-10">
+    <div className="bg-primary-200 rounded-xl w-full max-w-6xl mx-auto py-10 px-4">
+      <div className="container mx-auto">
         <Link href="/user/send-package">
-          <div className="grid grid-cols-12 items-center">
+          <div className="grid grid-cols-12 items-center gap-4">
             <div className="col-span-12 md:col-span-6 lg:col-span-5 p-4 md:p-6 lg:p-8">
-              <h1 className="text-4xl sm:text-4xl md:text-2xl lg:text-2xl text-primary-600 font-bold mb-4">
+              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl text-primary-600 font-bold mb-4">
                 Send Package
               </h1>
-              <p className="text-base sm:text-base md:text-base lg:text-base text-gray-700 mb-5">
+              <p className="text-base sm:text-base md:text-lg lg:text-lg text-gray-700 mb-5">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               </p>
-              <div className="bg-white rounded-xl w-10 grid place-items-center h-8">
-                <ArrowRightIcon className="text-primary-600  text-3xl  font-bold " />
-              </div>{" "}
+              <div className="bg-white rounded-xl w-10 h-10 flex justify-center items-center">
+                <ArrowRightIcon className="text-primary-600 text-3xl font-bold" />
+              </div>
             </div>
             <PaymentSheet />
-
-            <div className="col-span-12 md:col-span-6 lg:col-span-7">
-              <Image
-                className="object-contain max-w-full max-h-full"
-                src="/user/package.svg"
-                alt="team"
-                width={1500}
-                height={1500}
-              />
+            <div className="col-span-12 md:col-span-6 lg:col-span-7 flex justify-center items-center">
+              <div className="w-full max-w-md">
+                <Image
+                  className="object-contain"
+                  src="/user/package.svg"
+                  alt="team"
+                  width={600}
+                  height={600}
+                  layout="responsive"
+                />
+              </div>
             </div>
           </div>
         </Link>
