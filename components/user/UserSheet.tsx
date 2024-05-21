6ee14ae51adfafc67e1589e00bd1ae66330479c2
MadFury94 <onochieazukaeme@gmail.com>
2024-05-21 11:39:25 +0100

    change

--- components/user/UserSheet.tsx
+++ components/user/UserSheet.tsx
@@ -11,6 +11,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import SheetWelcome from "./SheetWelcome"
 
 export default function UserSheet() {
   return (
@@ -19,27 +20,14 @@ export default function UserSheet() {
       <SheetTrigger asChild>
         <Button variant="outline">Open</Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent >
         <SheetHeader>
           <SheetTitle>Edit profile</SheetTitle>
           <SheetDescription>
             Make changes to your profile here. Click save when you're done.
           </SheetDescription>
         </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
+      <SheetWelcome/>
         <SheetFooter>
           <SheetClose asChild>
             <Button type="submit">Save changes</Button>
