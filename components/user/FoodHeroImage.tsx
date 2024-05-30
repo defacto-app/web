fff4765ca744dfb740c56a16b2de57af382bcac8
MadFury94 <onochieazukaeme@gmail.com>
2024-05-30 16:06:07 +0100

    order section

--- components/user/FoodHeroImage.tsx
+++ components/user/FoodHeroImage.tsx
@@ -1,38 +1,41 @@
+"use client"
 import Image from "next/image";
 import React from "react";
 import { ArrowRightIcon, CircleArrowRight } from "lucide-react";
-import Link from "next/link";
+import UserSheet from "./UserSheet";
 
 export default function FoodHeroImage() {
   return (
-    <div className="bg-primary-700 rounded-xl w-[600px] h-[300px]">
-      <div className="container mx-auto py-10">
-        <Link href="/user/send-package">
-          <div className="grid grid-cols-12 items-center">
-            <div className="col-span-12 md:col-span-6 lg:col-span-5 p-4 md:p-6 lg:p-8">
-              <h1 className="text-4xl sm:text-4xl md:text-2xl lg:text-2xl text-primary-200 font-bold mb-4">
-                Coming Soon
+    <div>
+      <UserSheet>
+      <div className="relative cursor-pointer">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="absolute inset-0">
+              <Image
+                className="object-cover w-full h-full object-center"
+                src="/box2.jpg"
+                alt="Background"
+                layout="fill"
+                quality={100}
+              />
+              <div className="absolute inset-0 bg-primary-700 mix-blend-multiply" />
+            </div>
+            <div className="relative px-6 py-10 sm:py-20 lg:px-8 ">
+              <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                <span className="block text-gray-100">Package</span>
+                <span className="block text-primary-100">Delivery</span>
               </h1>
-              <p className="text-base sm:text-base md:text-base lg:text-base text-gray-200 mb-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.fwiknf nsaious
-              </p>
-              <div className="bg-primary-200 rounded-xl w-10 grid place-items-center h-8">
-              <ArrowRightIcon className="text-primary-600  text-3xl  font-bold " />
+              <div className="grid place-content-center">
 
               </div>
-            </div>
-            <div className="col-span-12 md:col-span-6 lg:col-span-7">
-              <Image
-                className="object-contain max-w-full max-h-full"
-                src="/user/package.svg"
-                alt="team"
-                width={1500}
-                height={1500}
-              />
+
             </div>
           </div>
-        </Link>
+        </div>
       </div>
+      </UserSheet>
+
     </div>
   );
 }
