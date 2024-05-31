6fc4081ef221a7f4907488e1ded22f1d2ecc90d1
MadFury94 <onochieazukaeme@gmail.com>
2024-05-31 20:59:48 +0100

    added package steps

--- components/SendReceive.tsx
+++ components/SendReceive.tsx
@@ -1,33 +1,52 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import { Button } from "@/components/ui/button";
+import React from 'react';
+import { usePackageContext } from "@/app/provider/packages.context";
 
-export default function SendReceive() {
+const SendReceive: React.FC = () => {
+  const { setCurrentStep } = usePackageContext();
+
+  const handleSubmit = () => {
+    console.log("Send package");
+  };
+
+  const handleSendClick = () => {
+    setCurrentStep("send");
+  };
+
   return (
-    <div>   <div className="relative cursor-pointer mt-10">
+    <div>
+      <div className="relative cursor-pointer mt-10">
         <div className="mx-auto max-w-7xl sm:px-4 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <Image
-                className="object-cover w-full h-full object-center"
-                src="/sending.jpg"
-                alt="Background"
-                layout="fill"
-                quality={100}
-              />
-              <div className="absolute inset-0 bg-secondary-700 mix-blend-multiply" />
-            </div>
-            <div className="relative px-6 py-10 sm:py-20 lg:px-8 ">
-              <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
-                <span className="block text-gray-100">Send</span>
-                <span className="block text-primary-100">Package</span>
-              </h1>
-              <div className="grid place-content-center">
-
+          <button
+            onClick={handleSendClick}
+            className='w-full shadow-xl sm:overflow-hidden sm:rounded-2xl'
+            type="submit"
+          >
+            <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+              <div className="absolute inset-0">
+                <Image
+                  className="object-cover w-full h-full object-center"
+                  src="/sending.jpg"
+                  alt="Background"
+                  layout="fill"
+                  quality={100}
+                />
+                <div className="absolute inset-0 bg-secondary-700 mix-blend-multiply" />
               </div>
-
+              <div className="relative px-6 py-10 sm:py-20 lg:px-8 ">
+                <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                  <span className="block text-gray-100">Send</span>
+                  <span className="block text-primary-100">Package</span>
+                </h1>
+                <div className="grid place-content-center"></div>
+              </div>
             </div>
-          </div>
+          </button>
         </div>
-      </div></div>
-  )
-}
+      </div>
+    </div>
+  );
+};
+
+export default SendReceive;
