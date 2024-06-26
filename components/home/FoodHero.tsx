03e8256ed7293a0b5d024b6638f5368d373a2673
MadFury94 <onochieazukaeme@gmail.com>
2024-06-26 20:31:19 +0100

    20%

--- components/home/FoodHero.tsx
+++ components/home/FoodHero.tsx
@@ -9,16 +9,16 @@ function FoodHero() {
     <div>
       <main>
         <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pb-14 lg:pt-8">
-          <div className="mx-auto max-w-7xl ">
+          <div>
             <div className="lg:grid lg:grid-cols-9">
               <div className="mx-auto max-w-md px-6 col-span-3 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                <div className="lg:py-24">
+                <div className="lg:py-2 container">
                   <div className="hidden sm:mb-5 sm:flex sm:justify-center lg:justify-start">
                     <a
                       href="#"
                       className="flex items-center rounded-full bg-white p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
                     >
-                      <span className="rounded-full text-primary-500 px-3 py-0.5 text-sm font-semibold leading-5 ">
+                      <span className="rounded-full text-primary-500 px-3 py-0.5 text-sm font-semibold leading-5">
                         Order from Restaurant
                       </span>
                       <span className="ml-4 text-sm text-gray-800">
@@ -31,12 +31,8 @@ function FoodHero() {
                     </a>
                   </div>
                   <h1 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-6xl lg:mt-6 xl:text-6xl">
-                    <span className="block text-primary-600">
-                      Feast your Senses
-                    </span>
-                    <span className="block text-primary-400">
-                      Fast and Fresh
-                    </span>
+                    <span className="block text-primary-600">Feast your Senses</span>
+                    <span className="block text-primary-400">Fast and Fresh</span>
                   </h1>
                   <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                     Enter your address to see what we deliver
@@ -65,18 +61,9 @@ function FoodHero() {
                 </div>
               </div>
 
-              <div className="-mb-16 mt-12 sm:-mb-48 lg:relative col-span-3 lg:m-0">
-                <div className="absolute inset-y-0 left-0 h-full w-full">
-                  <Image
-                    src="/hero/bg1.png"
-                    alt="Background"
-                    className="h-full w-full object-cover opacity-20"
-                    width={1000}
-                    height={1000}
-                  />
-                </div>
-                <div className=" mx-auto ml-44 max-w-md px-6 sm:max-w-2xl lg:max-w-none lg:px-0  ">
-                  <Image
+              <div className="-mb-16 relative mt-12 ml-10 sm:-mb-48 lg:relative col-span-3 lg:m-0">
+                <div className="absolute inset-0">
+                <Image
                     src="/hero/bg2.png"
                     alt="Foreground"
                     className="w-full lg:absolute lg:inset-y-0 lg:h-full lg:w-auto lg:max-w-none"
@@ -84,8 +71,32 @@ function FoodHero() {
                     height={1000}
                   />
                 </div>
+                <div className=" mx-auto ml-24 max-w-md px-6 sm:max-w-2xl lg:max-w-none lg:px-0">
+
+                   <Image
+                    src="/hero/bg1.png"
+                    alt="Background"
+                    className="h-full w-full object-cover opacity-150 "
+                    layout="fill"
+                  />
+                </div>
               </div>
-              <div className="col-span-3 bg-red-600">App</div>
+
+              <div className="col-span-3 grid justify-items-center bg-primary-600 w-full rounded-tl-full">
+                <div className="px-4 mt-40 ml-20">
+                  <h1 className="lg:px-3 font-bold text-xl grid justify-items-center text-gray-100">
+                    Download the Defacto App
+                  </h1>
+                  <br />
+                  <h1 className="text-xl text-gray-100 font">
+                    for faster ordering
+                  </h1>
+                </div>
+                <div>
+                  <div>Download on Playstore</div>
+                  <div>Download on App Store</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
