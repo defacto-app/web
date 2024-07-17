4724ea73529192b6759432aa7bc63d7c54364c14
sam.nmeje <sam@katalyst.tech>
2024-07-17 11:52:14 +0100

    fetch restaurant data

--- http/endpoints.ts
+++ http/endpoints.ts
@@ -1,20 +1,24 @@
 
 
 
-export const $api ={
-    guest:{
-        restaurant:{
+export const $api = {
+    guest: {
+        restaurant: {
             all: async () => {
                 try {
-
+                    const res = await fetch('https://api.defactoapp.com.ng/api/v1/restaurant/all?page=1&perPage=10');
+                    if (!res.ok) {
+                        throw new Error('Failed to fetch data');
+                    }
+                    return res.json();
                 } catch (error) {
                     console.log(error);
                     throw error; // Re-throw the error to handle it outside if needed
                 }
             },
         },
-    }
-}
+    },
+};
 
 
 
