2c755e0c7a01e2a9809d8f931e3b87d62bce69b0
sam.nmeje <sam@katalyst.tech>
2024-07-26 13:36:13 +0100

    safe work

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -11,27 +11,26 @@ import RestaurantArea from "@/components/home/RestaurantArea";
 import NewsletterSection from "@/components/home/NewsletterSection";
 
 export default function HomePage() {
-  return (
-    <div className="bg-[#FFFBFE] ">
-
-      {/* <HeroSection/> */}
-      {/* <FoodHero/> */}
-      <FoodHero2/>
-      <RestaurantArea/>
-      <div className="text-center bg-primary-900 pt-10 pb-10">
-        <h1 className="text-xl pb-5 font-bold text-primary-200">Our Reviews</h1>
-        <h2 className="text-4xl font-bold text-primary-200">Our Happy Customers</h2>
-      </div>
-      <MarqueeSlide/>
-      {/*<NewsletterSection/>*/}
-      {/* <SendReceive/>
+	return (
+		<div className="bg-[#FFFBFE] ">
+			{/* <HeroSection/> */}
+			{/* <FoodHero/> */}
+			<FoodHero2 />
+			<RestaurantArea />
+			<div className="text-center bg-primary-900 pt-10 pb-10">
+				<h1 className="text-xl pb-5 font-bold text-primary-200">Our Reviews</h1>
+				<h2 className="text-4xl font-bold text-primary-200">
+					Our Happy Customers
+				</h2>
+			</div>
+			<MarqueeSlide />
+			{/*<NewsletterSection/>*/}
+			{/* <SendReceive/>
       <CtaSection/>
       <BecomeRider/> */}
 
-      {/* <FaqSection/> */}
-
-
-    </div>
-  );
+			{/* <FaqSection/> */}
+		</div>
+	);
 }
-export const runtime = 'edge';
+export const runtime = "edge";
