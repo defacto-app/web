409319cdd4bbbf67b38734260fcdd8251c57244d
sam.nmeje <sam@katalyst.tech>
2024-09-07 01:16:07 +0100

    fix build issue

--- app/admin/x/layout.tsx
+++ app/admin/x/layout.tsx
@@ -1,40 +1,36 @@
 "use client";
 
-import {Inter as FontSans} from "next/font/google";
-import {cn} from "@/lib/utils";
-import {AdminFooter, AdminHeader} from "@/app/admin/components/admin.header";
-import { ToastContainer } from 'react-toastify';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactNode, useState } from 'react';
-import 'react-toastify/dist/ReactToastify.css';
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { AdminFooter, AdminHeader } from "@/app/admin/components/admin.header";
+import { ToastContainer } from "react-toastify";
+import { ReactNode, useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
 
 const fontSans = FontSans({
-    subsets: ["latin"],
-    variable: "--font-sans",
+	subsets: ["latin"],
+	variable: "--font-sans",
 });
 
 export default function AdminRootLayout({
-                                            children,
-                                        }: Readonly<{
-    children: React.ReactNode;
+	children,
+}: Readonly<{
+	children: React.ReactNode;
 }>) {
+	return (
+		<div lang="en">
+			<div
+				className={cn("bg-background font-sans antialiased", fontSans.variable)}
+			>
+				<AdminHeader />
+				<ToastContainer position="top-center" />
 
-    const [queryClient] = useState(() => new QueryClient());
+				<div>{children}</div>
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <div lang="en">
-                <div className={cn("bg-background font-sans antialiased", fontSans.variable)}>
-                    <AdminHeader/>
-                    <ToastContainer position="top-center"/>
-
-                    <div>{children}</div>
-
-                    <AdminFooter/>
-                </div>
-            </div>
-        </QueryClientProvider>
-    );
+				<AdminFooter />
+			</div>
+		</div>
+	);
 }
 
-export const runtime = 'edge';
+export const runtime = "edge";
