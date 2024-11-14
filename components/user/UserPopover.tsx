34671343e0e32a148b7d8ff310259c5ef256210b
sam.nmeje <sam@katalyst.tech>
2024-11-14 13:07:26 +0000

    deleted user pop over

--- components/user/UserPopover.tsx
+++ components/user/UserPopover.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import UserPopoverItem from "./UserPopoverItem";
-import { ChevronDown, UserIcon, X } from "lucide-react";
+import { ChevronDown, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useAtomAuthContext } from "@/app/store/authAtom";
 
@@ -9,9 +8,23 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
+import { useRouter } from "next/navigation";
+import { truncateText } from "@/utils";
+import Link from "next/link";
+import EditUserModal from "@/components/user/EditUserModal";
+import EditUserNumberModal from "@/components/user/EditUserNumberModal";
 
 export default function UserPopover() {
-	const { authUser } = useAtomAuthContext();
+	const { logOut, authUser, setModalOpen } = useAtomAuthContext();
+	const router = useRouter();
+	async function logout() {
+		await fetch(`/api/auth/logout`);
+
+		router.push("/");
+		await logOut();
+
+		setModalOpen(false);
+	}
 
 	return (
 		<div>
@@ -28,9 +41,74 @@ export default function UserPopover() {
 					</Button>
 				</PopoverTrigger>
 				<PopoverContent className="w-80">
-					<UserPopoverItem />
+					<div>
+						<div className="">
+							<h1 className="mb-4 font-bold text-xl">
+								Hello{" "}
+								{truncateText(
+									authUser.firstName || authUser.email || authUser.phoneNumber,
+									15,
+								)}
+							</h1>
+
+							<div className="border-b border-gray-500" />
+						</div>
+
+						<div className={`flex flex-col gap-y-4 mt-4`}>
+							<Link href={`/user/account`}>Account</Link>
+							<Link href={`/user/order/history`}>Order history</Link>
+							<Button
+								onClick={logout}
+								variant={`outline`}
+								className="text-red-500 font-bold w-40"
+							>
+								Log out
+							</Button>
+						</div>
+
+						<div className="flex justify-between mb-7 mt-4">
+							<div className="flex flex-col gap-y-2">
+								<div className="">
+									<h1 className="mt-2 text-gray-500">Name</h1>
+									<h2>{authUser.firstName || authUser.email}</h2>
+								</div>
+								<div>
+									<h1 className="text-gray-500">Email</h1>
+									<h1>{authUser.email}</h1>
+								</div>
+							</div>
+							<div className="mt-2">
+								<EditUserModal />
+							</div>
+						</div>
+						<div className="border-b border-gray-500" />
+						<div>
+							<div className="flex justify-between mb-4">
+								<div>
+									<div>
+										<h1 className="mt-2 text-gray-500">Phone</h1>
+										<h2>{authUser.phoneNumber || "not set"}</h2>
+									</div>
+								</div>
+								<div className="mt-2">
+									<EditUserNumberModal />
+								</div>
+							</div>
+							<div className="border-b mb-2 mt-2 border-gray-500" />
+						</div>
+						<div className="flex justify-between mb-4">
+							<div>
+								<div />
+							</div>
+							<div></div>
+						</div>
+					</div>
 				</PopoverContent>
 			</Popover>
 		</div>
 	);
 }
+
+// user popover
+
+// user menu
